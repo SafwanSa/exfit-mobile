fd72b65f8fb233de11ae6a8d1e1dd77f590323cf
Safwan <safwan9f@gmail.com>
2021-03-18 22:47:16 +0300

    Implement clickable exercises widget

--- lib/widgets/exercise.dart
+++ lib/widgets/exercise.dart
@@ -1,3 +1,4 @@
+import 'package:exfit/animations/bounce.dart';
 import 'package:exfit/models/exercise.dart';
 import 'package:flutter/material.dart';
 
@@ -16,43 +17,55 @@ class ExerciseWidget extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 10),
         child: Row(
           children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                exercise.imagesUrls[0],
+            Expanded(
+              flex: 1,
+              child: Bounce(
+                onTap: () {},
+                child: Container(
+                  color: Colors.transparent,
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          exercise.imagesUrls[0],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              exercise.name,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 15),
+                            Text(
+                              exercise.difficulty,
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black38,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    exercise.name,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    exercise.difficulty,
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.black38,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Spacer(),
-            IconButton(
-              icon: Icon(
+            Bounce(
+              onTap: () {},
+              child: Icon(
                 Icons.add_box_rounded,
                 size: 45,
                 color: Theme.of(context).primaryColor,
               ),
-              onPressed: () {},
             )
           ],
         ),
